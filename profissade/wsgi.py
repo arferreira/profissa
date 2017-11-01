fdcce069f15d632bb458e4e525585be5e03cba56
Antonio Ricardo <antonioricardoarfs@gmail.com>
2017-10-31 22:24:14 -0300

    Add Cling and dj_static to provide static files on heroku

--- profissade/wsgi.py
+++ profissade/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+# working with staticfiles on heroku
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "profissade.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
